2f88268a499b345565be5393d9368c53ad0fa502
Hanai <ihanai1991@gmail.com>
2020-05-14 00:31:38 +0800

    update weex code

--- src/page-main/weex/styles.ts
+++ src/page-main/weex/styles.ts
@@ -3,7 +3,6 @@ import { CSSProperties } from '@rax-types/rax';
 const styles: { [key: string]: CSSProperties } = {
   container: {
     flex: 1,
-    overflow: 'hidden',
   },
   scrollView: {
     flex: 1,
