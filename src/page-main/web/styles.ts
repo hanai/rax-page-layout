5ee4e71cbc382371fa1fe6774037048d947f03b7
Hanai <ihanai1991@gmail.com>
2020-05-22 21:59:22 +0800

    update

--- src/page-main/web/styles.ts
+++ src/page-main/web/styles.ts
@@ -1,10 +1,12 @@
 import { CSSProperties } from '@rax-types/rax';
+import { isIOS } from '../../utils/ua';
 
 const styles: { [key: string]: CSSProperties } = {
   container: {
     flex: 1,
     overflowY: 'scroll',
     overflowX: 'hidden',
+    ...(isIOS() ? { WebkitOverflowScrolling: 'touch' } : null),
   },
   transformView: {
     overflow: 'visible',
--- src/utils/ua.ts
+++ src/utils/ua.ts
@@ -1,2 +1,4 @@
 export const isFirefox = () =>
   window.navigator.userAgent.indexOf('Firefox') > -1;
+
+export const isIOS = () => /iPad|iPhone|iPod/.test(window.navigator.userAgent);
